68fd8259102a256154e8d35bc947594988b7be1c
ohio-math <fschwieb@protonmail.com>
2025-09-24 19:09:56 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,18 @@
 // Script to be injected
 
 self.addEventListener('fetch', event => {
-  const encode = (input) => encodeURIComponent(input);
   const url = event.request.url;
   const compiled = new URL(event.request.url);
+  
+  const registeredBy = "ohio-math.github.io";
+  if (compiled.hostname == registeredBy) {
+    event.respondWith(fetch(event.request));
+  }
+  
+  const encode = (input) => encodeURIComponent(input);
 
   // Example: proxy requests to /api/* to another server
-  if (url.startsWith("http") {
+  if (url.startsWith("http")) {
     const newUrl = 'https://fschwieb-2597dad56586.herokuapp.com/prox?url=' + encode( url );
     event.respondWith(fetch(newUrl));
   } else {
